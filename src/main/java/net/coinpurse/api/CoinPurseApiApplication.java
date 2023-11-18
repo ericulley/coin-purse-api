0af8d601fce6f7b57e49757c85a5c32bf85b5e6b
Eric Culley <ericulley@gmail.com>
2023-11-17 23:08:27 -0700

    mongo env variable

--- src/main/java/net/coinpurse/api/CoinPurseApiApplication.java
+++ src/main/java/net/coinpurse/api/CoinPurseApiApplication.java
@@ -1,11 +1,12 @@
 package net.coinpurse.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("net.coinpurse")
 public class CoinPurseApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CoinPurseApiApplication.class, args);
