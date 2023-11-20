66d658e4a5f0ca8e20035892022a1713e4fc230f
Eric Culley <ericulley@gmail.com>
2023-11-19 22:12:27 -0700

    mvn build

--- src/main/java/net/coinpurse/api/config/MongoConfig.java
+++ src/main/java/net/coinpurse/api/config/MongoConfig.java
@@ -4,14 +4,9 @@ import com.mongodb.MongoException;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoDatabase;
-import org.bson.BsonDocument;
-import org.bson.BsonInt64;
-import org.bson.Document;
-import org.bson.conversions.Bson;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
 
 @Configuration
@@ -27,24 +22,16 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
         return mongoDatabaseName;
     }
 
-    @Override
-    public MongoClient mongoClient() {
-        // Replace the placeholder with your Atlas connection string
-        String uri = mongoURI;
+    @Bean
+    public MongoClient mongoClient() throws MongoException {
         // Create a new client and connect to the server
-        System.out.println("Mongo Connection String: " + uri);
-        try (
-                MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("coin-purse-db");
-            try {
-                // Send a ping to confirm a successful connection
-                Bson command = new BsonDocument("ping", new BsonInt64(1));
-                Document commandResult = database.runCommand(command);
-                System.out.println("Pinged your deployment. You successfully connected to MongoDB! " + commandResult);
-            } catch (MongoException me) {
-                System.err.println(me);
-            }
+        try {
+            MongoClient mongoClient = MongoClients.create(mongoURI);
+            MongoDatabase database = mongoClient.getDatabase(mongoDatabaseName);
             return mongoClient;
+        } catch (MongoException e) {
+            System.err.println(e);
+            throw e;
         }
     }
 }
