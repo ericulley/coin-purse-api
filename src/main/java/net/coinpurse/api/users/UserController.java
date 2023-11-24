fc8568f3f5e07959965a40c99acc2ad8dc2ff46d
Eric Culley <ericulley@gmail.com>
2023-11-24 13:56:43 -0700

    updated user controller

--- src/main/java/net/coinpurse/api/users/UserController.java
+++ src/main/java/net/coinpurse/api/users/UserController.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v2/users")
-@CrossOrigin
+@CrossOrigin("*")
 public class UserController {
 
     @Autowired
--- src/main/java/net/coinpurse/api/users/UserRepository.java
+++ src/main/java/net/coinpurse/api/users/UserRepository.java
@@ -2,10 +2,12 @@ package net.coinpurse.api.users;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.Optional;
 
 @Repository("userRepository")
+@CrossOrigin
 interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findUserByEmail(String email);
 }
